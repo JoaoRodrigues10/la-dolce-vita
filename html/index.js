4bff64d0550968d8ef04b5f1a29fac5f72832328
JoaoRodrigues10 <joaopedr8945@gmail.com>
2024-02-28 17:20:22 -0300

    ok

--- html/index.js
+++ html/index.js
@@ -1,15 +1,14 @@
-// Abre o modal quando o ícone do carrinho é clicado
+
 document.getElementById('carrinho-icon').addEventListener('click', function(event) {
     event.stopPropagation(); // Evita que o clique se propague para o contêiner pai
     document.getElementById('myModal').style.display = 'block';
 });
 
-// Fecha o modal quando o usuário clica no botão de fechar (×)
+
 document.querySelector('.close').addEventListener('click', function() {
     document.getElementById('myModal').style.display = 'none';
 });
 
-// Fecha o modal quando o usuário clica fora da área do modal
 window.addEventListener('click', function(event) {
     if (event.target == document.getElementById('myModal')) {
         document.getElementById('myModal').style.display = 'none';
