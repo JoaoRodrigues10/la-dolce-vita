1e0264454cd632045a798fa55818df65e8a2e3af
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2024-04-12 19:58:36 -0300

    LAB 1
    Com a parte de HTML e CSS criadas pelo caio como index do projeto

---
+++ LaDolceVita/src/main/java/br/com/LaDolceVita/servlet/HomePageServlet.java
@@ -0,0 +1,14 @@
+package br.com.LaDolceVita.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/Home-page")
+public class HomePageServlet extends HttpServlet {
+
+
+}
